55afcb32919f8bb8e53c86dcb32c97ecb3fd5ddc
thunligh <thunlighgaming@gmail.com>
2023-10-25 09:29:14 -0500

    moderation commands

---
+++ src/commands/moderation/ban.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('discord.js')
+const Discord = require("discord.js");
+const emj = require("../../botconfig/emojis.json");
+const emb = require("../../botconfig/embed.json");
+module.exports = {
+    cmd: new SlashCommandBuilder()
+        .setName('ban')
+        .setDescription('Banea a un usuario del servidor')
+        .addUserOption(option => option.setName('usuario').setDescription('El usuario que quieres banear.').setRequired(true))
+        .addStringOption(option => option.setName('razón').setDescription('La razón por la que quieres banear al usuario.').setRequired(false)),
+
+    owner: true,
+    permissions_bot: ["Ban_Members"],
+    permissions_member: ["Ban_Members"],
+    requiredroles: [],
+
+    async execute(client, interaction) {
+        const { options, guild } = interaction;
+
+        const user = interaction.options.getUser('usuario');
+        const reason = interaction.options.getString('razón') || 'No especificado';
+
+        try {
+            const bannedUsers = await guild.bans.fetch();
+            if (bannedUsers.has(user.id)) return interaction.reply(`${emj.utility.error} **El usuario ya se encuentra baneado**`)
+
+            await interaction.guild.members.ban(user, { reason });
+            return interaction.reply({ content: `Usuario ${user.tag} baneado por ${interaction.user.tag}. Razón: ${reason}`, ephemeral: true });
+        } catch (error) {
+            console.error(error);
+            return interaction.reply({ content: 'Ocurrió un error al intentar desbanear al usuario.', ephemeral: true });
+        }
+    }
+}
---
+++ src/commands/moderation/unban.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('discord.js')
+const Discord = require("discord.js");
+const emj = require("../../botconfig/emojis.json");
+const emb = require("../../botconfig/embed.json");
+module.exports = {
+    cmd: new SlashCommandBuilder()
+        .setName('unban')
+        .setDescription('Desbanea a un usaurio del servidor')
+        .addUserOption(option => option.setName('usuario').setDescription('El usuario que quieres desbanear.').setRequired(true))
+        .addStringOption(option => option.setName('razón').setDescription('La razón por la que quieres desbanear al usuario.').setRequired(false)),
+
+    owner: true,
+    permissions_bot: ["Ban_Members"],
+    permissions_member: ["Ban_Members"],
+    requiredroles: [],
+
+    async execute(client, interaction) {
+        const { options, guild } = interaction;
+
+        const user = options.getUser('usuario');
+        const reason = options.getString('razón') || 'No especificado';
+
+        try {
+            const bannedUsers = await guild.bans.fetch();
+            if (!bannedUsers.has(user.id)) return interaction.reply(`${emj.utility.error} **El usuario no se encuentra baneado**`)
+
+            await guild.members.unban(user, { reason });
+            return interaction.reply({ content: `Usuario ${user.tag} desbaneado por ${interaction.user.tag}. Razón: ${reason}`, ephemeral: true });
+        } catch (error) {
+            console.error(error);
+            return interaction.reply({ content: 'Ocurrió un error al intentar desbanear al usuario.', ephemeral: true });
+        }
+    }
+}
